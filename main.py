977bc5fe2151573724a4e75eeb6a63a2ed579c52
Maximiliano Militzer <maxmilitzer@gmail.com>
2025-04-09 13:03:07 +0200

    añadir suma

---
+++ main.py
@@ -0,0 +1,12 @@
+def sumar(a, b):
+    """
+    Función que suma dos números y devuelve el resultado.
+
+    Args:
+        a: Primer número.
+        b: Segundo número.
+
+    Returns:
+        La suma de los dos números.
+    """
+    return a + b
